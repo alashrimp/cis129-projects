3aad03d15e72b24aed50270f710d7ea5a3902280
alashrimp <160691184+alashrimp@users.noreply.github.com>
2024-02-20 20:56:30 -0700

    Updated cis129_lab02_shortStory.py by following step 5 of lab

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Kait Goff
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+'there are songs sang by birds that are lost'
+"the tune in which no words are even sung"
+'the hope is created by the whispers of the wind and the silence thereafter'
