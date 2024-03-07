76f40942b175e72539a2bd440ab3979958be0fce
alashrimp <160691184+alashrimp@users.noreply.github.com>
2024-03-06 23:34:48 -0700

    Create cis129_lab03_coffeeShop.py

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,41 @@
+# Author : Kaitlynn G. 
+# Lab 3 in CIS129 course at PCC
+# Short 'My Coffee Shop' simulator program 
+
+print('Coffee and Muffin Shop')
+
+# Define prices
+"Coffee" : 5.00, 
+"Muffin" : 4.00,
+
+cost_coffee == 5.00
+cost_muffin == 4.00
+
+Tax_rate : 0.06
+
+# Get user input
+
+input(int("How many coffees would you like?"))
+int(1)
+
+input(int(How many muffins would you like?"))
+int(2)
+
+# Calculate Subtotal
+
+int(cost_muffin + cost_coffee) * str(Tax_rate) = (tax_of_purchase)
+int(tax_of_purchase + int(cost_muffin + cost_coffee) = grandtotal 
+
+
+print(*****************************)
+print('My Coffee and Muffin Shop")
+print(*****************************)
+
+cost_coffee * 2 == 10.00
+cost_muffin * 1 == 4.00
+
+subtotal : (cost_muffin + cost_coffee) * tax_of_purchase =
+
+grandtotal =
+print(Thank you for coming, we hope to see you again soon!')
+print(*****************************)
