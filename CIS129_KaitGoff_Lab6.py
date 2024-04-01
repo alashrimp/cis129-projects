c064da081c1c6247f43ec44451a1b678a7272a3c
alashrimp <160691184+alashrimp@users.noreply.github.com>
2024-03-31 20:43:22 -0700

    Update CIS129_KaitGoff_Lab6.py

--- CIS129_KaitGoff_Lab6.py
+++ CIS129_KaitGoff_Lab6.py
@@ -1,1 +1,34 @@
+# Kaitlynn Goff
+# 31 March 2024 
+# CIS129 Lab 6
+# This code is a Hot Dog Cookout Calculator 
+
+#Define variables 
+
+hot_dogs = 10 # number of hot dogs in a package 
+hot_dog_buns = 8 #number of buns in a package 
+
+Need to: 
+Calculate # of packages of hot dogs 
+Calculate # of hot dog buns 
+Need to: ask user for number of people attending the cookout 
+ask user number of hot dogs each person will be given 
+Need to diplay:
+1. The minimum number of packages of hot dogs required.
+2. The minimum number of packages of buns required
+3. The number of hot dogs that will be left over
+4. The number of buns that will be left ove
+
+
+Module main()
+
+#Input from user 
+print("How many people will be coming to the cookout?", ":")
+num_of_People = int(input())
+print(How many hotdogs will each person be given?", ":")
+num_of_Meals = int(input())
+dogs_Left = 
+buns_Left
+min_Dogs
+min_Buns
 
