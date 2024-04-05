c868508bf217261a67a507ad7ddd1424eb985782
alashrimp <160691184+alashrimp@users.noreply.github.com>
2024-04-04 21:41:22 -0700

    Update CIS129_KaitGoff_Lab6.py

--- CIS129_KaitGoff_Lab6.py
+++ CIS129_KaitGoff_Lab6.py
@@ -3,30 +3,30 @@
 # CIS129 Lab 6
 # This code is a Hot Dog Cookout Calculator 
 
-#Import library 
 import math
 
-#Define variables 
-hot_Dogs = 10 # number of hot dogs in a package 
-Buns = 8 # number of buns in a package
+def gettotal_Hotdogs():
+    print("Enter the number of people attending the cookout",":")
+    num_of_people = int(input())
+    print("Enter the number of hot dogs for each person:")
+    hotdogs_per_person = int(input())
+    total_Hotdogs =  num_of_people *  hotdogs_per_person
+    return total_Hotdogs 
 
-#Input from user 
-print("Enter the number of people attending the cookout",":")
-num_of_people = int(input()) # number of people attending cookout
-print("Enter the number of hot dogs for each person:")
-hotdogs_per_person = int(input()) # number of hot dogs per person
-total_Hotdogs = num_of_people * hotdogs_per_person
+def showresults(total_Hotdogs):
+    hot_Dogs = 10
+    Buns = 8 
+    
+    min_Dogs = math.ceil(total_Hotdogs / hot_Dogs)
+    dogs_Left = (hot_Dogs - total_Hotdogs % hot_Dogs) % hot_Dogs # left over hot dogs
+    
+    min_Buns = math.ceil(total_Hotdogs / Buns)
+    buns_Left = (Buns - total_Hotdogs % Buns) % Buns # left over hot dog buns
 
-#Calculating hot dogs needed
-min_Dogs = math.ceil(total_Hotdogs / hot_Dogs)
-dogs_Left = (hot_Dogs - total_Hotdogs % hot_Dogs) % hot_Dogs # left over hot dogs
-
-#Calculating hot dog buns needed 
-min_Dogs = math.ceil(total_Hotdogs / hot_Dogs) # minimum hotdogs needed 
-buns_Left = (Buns - total_Hotdogs % Buns) % Buns # left over hot dog buns
-min_Buns = math.ceil(total_Hotdogs / Buns)
-
-print("Minimum packages of hot dogs needed: ", min_Dogs)
-print("Minimum packages of hot dogs buns needed: ", min_Buns)
-print("Hot dogs left over: ", dogs_Left)
-print("Hot dog buns left over: ", buns_Left)
+    print("Minimum packages of hot dogs needed: ", min_Dogs)
+    print("Minimum packages of hot dogs buns needed: ", min_Buns)
+    print("Hot dogs left over: ", dogs_Left)
+    print("Hot dog buns left over: ", buns_Left)
+    
+total_Hotdogs = gettotal_Hotdogs()
+showresults(total_Hotdogs)
